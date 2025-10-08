cd52011f5ddbeaa2414757e48787dbbe87009dbc
Aishraj30 <aishubamoriya@gmail.com>
2025-10-09 00:21:19 +0530

    Fix: added Render-ready server config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Approutes from './Routes/Approutes'
+//import Approutes from './Routes/Approutes'
+
 
 
 
 const App = () => {
   return (
     <div>
-      <Approutes/>
+     {/* // <Approutes/> */}
+     <Approutes/>
 
 
 
